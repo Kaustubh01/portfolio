d25b1f84138acc14cacd8cd0a8cec1eb83445fe8
kaustubh <kaustubhmayekar02@gmail.com>
2026-03-23 01:26:00 +0530

    added loading page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,60 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div
-      className='bg-neutral-100 w-full h-screen'
-    >
-      <h1 className='text-black text-2xl'>Kaustubh</h1>
+    <div className="w-full h-screen">
+
+      <AnimatePresence>
+        {loading && (
+          <motion.div
+            className='absolute inset-0 bg-neutral-100 flex items-center justify-center'
+            initial={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <h1 className="text-5xl font-bold text-gray-800 text-left">
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Hi!")
+                    .pauseFor(700)
+                    .typeString("<br/>I'm <span style='color:#ef4444'>Kaustubh</span>")
+                    .start();
+                }}
+                options={{ cursor: "" }}
+              />
+            </h1>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {/* MAIN CONTENT */}
+      {!loading && (
+        <motion.div
+          className="w-full h-screen flex items-center justify-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          
+        </motion.div>
+      )}
+
     </div>
   )
 }
 
-export default Home 
+export default Home
