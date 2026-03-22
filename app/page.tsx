3a7506493dfb7eae20f00edecf439ae07d114413
kaustubh <kaustubhmayekar02@gmail.com>
2026-03-23 00:33:26 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div
+      className='bg-neutral-100'
+    >
+      <h1>Kaustubh</h1>
+    </div>
+  )
+}
+
+export default Home 
